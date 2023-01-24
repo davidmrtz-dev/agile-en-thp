083f4ae7adbeb6a6b747bbbebb92b618b608ecb6
David Mtz <davidelimg1@gmail.com>
2023-01-24 10:42:24 -0600

    [CHORE]: Implemented basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css';
-
+import debounce from "lodash/debounce";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+const ITEMS_API_URL = 'https://example.com/api/items';
+const DEBOUNCE_DELAY = 500;
 
 function App() {
+  const [input, setInput] = useState('');
+
+  const handleChange = (e: any) => {
+    setInput(e.target.value);
+  };
+
+  useEffect(() => {
+    if (input) console.log('input:', input);
+  }, [input]);
+
   return (
     <div className="App">
       <div>
-        <input type='text' />
+        <input type='text' onChange={handleChange}/>
       </div>
       <div>
 
